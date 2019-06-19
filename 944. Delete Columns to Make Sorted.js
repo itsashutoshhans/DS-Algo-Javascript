f533bfbf779f2a7f95e9867a4536931699247b98
Hans <ahans@informatica.com>
2019-06-19 13:17:38 +0530

    adding comments for 944 solution

--- 944. Delete Columns to Make Sorted.js
+++ 944. Delete Columns to Make Sorted.js
@@ -9,6 +9,8 @@ var minDeletionSize = function(A) {
     const stringLength = A[0].length;
     const comparisonLength = A.length-1; // length of array
     let count = 0;
+    // for each character in the word compare all the words
+    // if they are not sorted that means the column needs to be deleted
     for (let i = 0; i < stringLength; i++) {
         for (let j = 0; j < comparisonLength; j++) {
            if(A[j][i] > A[j+1][i]) {
