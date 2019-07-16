8be3e1a3ab78ed7b9730bd2dc099fd2602a7ba90
ashutoshhans611 <ashutosh.hans@gmail.com>
2019-07-16 14:20:25 +0530

    Minimum Depth of Binary tree

---
+++ LeetCode/Easy/111. Minimum Depth of Binary Tree.js
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var minDepth = function(root) {
+    if(root === null) return 0;
+    if(!root.left) return 1 + minDepth(root.right);
+    if(!root.right) return 1 + minDepth(root.left);
+    return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+}
